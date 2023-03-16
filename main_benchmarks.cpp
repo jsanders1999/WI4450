26cf3ec693090c7655c954336933a5f13956ad1a
jsanders1999 <julian.arthur.sanders@gmail.com>
2023-03-16 17:12:19 +0100

    Timer now works! time to run it up to 48 threads

--- main_benchmarks.cpp
+++ main_benchmarks.cpp
@@ -87,8 +87,8 @@ int main(int argc, char* argv[])
   double resNorm, tol=std::sqrt(std::numeric_limits<double>::epsilon());
 
   //loop over thread numbers
-  for (int tn =1; tn<=4; tn++){
-    Timer timer("CG solver for" + std::to_string(tn));
+  for (int tn =1; tn<=48; tn++){
+    Timer timer("CG solver for " + std::to_string(tn) " threads");
     // solution vector: start with a 0 vector
     init(n, x, 0.0);
     try {
