6c08177b79d3074703968458536abaebee4fe9ea
jsanders1999 <julian.arthur.sanders@gmail.com>
2023-03-18 10:36:01 +0100

    strong scaling 32 threads

--- main_benchmarks.cpp
+++ main_benchmarks.cpp
@@ -88,7 +88,7 @@ int main(int argc, char* argv[])
   double resNorm, tol=std::sqrt(std::numeric_limits<double>::epsilon());
 
   //loop over thread numbers
-  for (int tn =1; tn<=1; tn+=2){
+  for (int tn =1; tn<=0; tn+=2){
     Timer timer("CG solver for " + std::to_string(tn) + " threads");
     // solution vector: start with a 0 vector
     init(n, x, 0.0);
@@ -112,8 +112,8 @@ int main(int argc, char* argv[])
 
   // Loop over nx size
   {
-  //int nx_arr[6] = {32, 64, 128, 256, 512, 1024};
-  int nx_arr[0] = {};
+  int nx_arr[6] = {32, 64, 128, 256, 512, 1024};
+  //int nx_arr[0] = {};
   for(int nx: nx_arr){
     std::cout<< nx <<std::endl;
     int ny = nx;
@@ -161,7 +161,7 @@ int main(int argc, char* argv[])
         }
 
     // solve the linear system of equations using CG
-    int numIter, maxIter=500;
+    int numIter, maxIter=2000;
     double resNorm, tol=std::sqrt(std::numeric_limits<double>::epsilon());
 
     //loop over thread numbers
