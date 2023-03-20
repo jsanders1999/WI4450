8186bd6177f4d1fc11b8814f41f21d75273f66eb
jsanders1999 <julian.arthur.sanders@gmail.com>
2023-03-20 17:57:28 +0100

    z-y-x loop

--- operations.cpp
+++ operations.cpp
@@ -82,9 +82,9 @@ void apply_stencil3d(stencil3d const* S,
 //#pragma omp parallel for 
   
   
-  for(int i = 0; i < S->nx; i++){
+  for(int k = 0; k < S->nz; k++){
     for(int j = 0; j < S->ny; j++){
-      for(int k = 0; k < S->nz; k++){
+      for(int i = 0; i < S->nx; i++){
         // Add the center value of the stencil
         v[S->index_c(i,j,k)] = S->value_c*u[S->index_c(i,j,k)];
 
