1be823043af166c8a09e6c74f577d21fa70270f3
jsanders1999 <julian.arthur.sanders@gmail.com>
2023-03-20 18:16:45 +0100

    8 cores

--- operations.cpp
+++ operations.cpp
@@ -80,7 +80,6 @@ void apply_stencil3d(stencil3d const* S,
   // A for loop over the three dimensions that applies the stencil S to vector u and stores it in v
   // Possible speedup improvement: Use case switching instead of in statements
   #pragma omp parallel for 
-  
   for(int k = 0; k < S->nz; k++){
     for(int j = 0; j < S->ny; j++){
       for(int i = 0; i < S->nx; i++){
